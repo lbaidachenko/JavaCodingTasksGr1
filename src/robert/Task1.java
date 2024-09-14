2bca2634e363ab5acb95e75e3c71af3f1fef0c0a
Saltymelon13 <71376459+Saltymelon13@users.noreply.github.com>
2024-09-14 19:48:40 +0900

    Odd or Even Task

--- src/robert/Task1.java
+++ src/robert/Task1.java
@@ -1,4 +1,13 @@
 package robert;
 
 public class Task1 {
+    public static void main(String[] args) {
+        int num = 0;
+
+        if (num % 2 == 0){
+            System.out.println( num + " # even");
+        }else {
+            System.out.println(num + " # odd");
+        }
+    }
 }
