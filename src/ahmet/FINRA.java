a430f15e9958dbeee466a8ee4ee1cfed4fada649
Jomolocco <ahmet23cemal@gmail.com>
2024-09-22 19:41:10 +0200

    add FINRA

--- src/ahmet/FINRA.java
+++ src/ahmet/FINRA.java
@@ -7,6 +7,8 @@ For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the nu
 
 public class FINRA {
     public static void main(String[] args) {
+        System.out.println();
+        /*
     for (int i = 1 ; i <= 30 ; i++) {
         if (i % 3 == 0){
             System.out.print(" "+"FIN");
@@ -18,5 +20,25 @@ public class FINRA {
             System.out.print(" "+i);
         }
     }
+
+         */
+        // create 1 up to 30 in a single line of code using a switch statement.
+        for (int i = 1 ; i <= 30 ; i++ ){
+            switch (i % 3 == 0&& i % 5 == 0 ? 3:i % 3 == 0 ? 1 : i % 5 == 0 ? 2 :  0){// 0- FIN, 1- FIN, 2- RA, 3- FINRA
+              case 0: // not divide by 3 and 5
+                  System.out.print(" "+i);
+                break;
+              case 1: // only divide by 3
+                System.out.print(" "+"FIN");
+                break;
+              case 2:// only divide by 5
+                  System.out.print(" "+"RA");
+                break;
+              case 3://  divide by 3 and 5
+                    System.out.print(" "+"FINRA");
+                break;
+            }
+        }
+        System.out.println();
     }
 }
