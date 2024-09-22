fb3855a5bc66312ed6cad6feed86ce48e6e0ca9c
Jomolocco <ahmet23cemal@gmail.com>
2024-09-22 02:01:26 +0200

    add FINRA

---
+++ src/ahmet/FINRA.java
@@ -0,0 +1,12 @@
+package ahmet;
+/*
+Write a function that prints out the numbers from 1 to 30 but for numbers that are a multiple of 3,
+print "FIN" instead of the number, and for numbers that are a multiple of 5, print "RA" instead of the number.
+For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the number.
+
+ */
+public class FINRA {
+    public static void main(String[] args) {
+
+    }
+}
