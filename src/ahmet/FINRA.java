8f599692b224923a25e7d9fcf3e428c7fed514a1
Jomolocco <ahmet23cemal@gmail.com>
2024-09-24 01:04:05 +0200

    add Text2

--- src/ahmet/FINRA.java
+++ src/ahmet/FINRA.java
@@ -8,6 +8,7 @@ For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the nu
 public class FINRA {
     public static void main(String[] args) {
         System.out.println();
+
         /*
     for (int i = 1 ; i <= 30 ; i++) {
         if (i % 3 == 0){
