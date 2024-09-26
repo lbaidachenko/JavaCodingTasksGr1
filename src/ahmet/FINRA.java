e0efd09341569c8bd42b771bcb2b064da902e956
Jomolocco <ahmet23cemal@gmail.com>
2024-09-26 19:35:38 +0200

    FiNRa

--- src/ahmet/FINRA.java
+++ src/ahmet/FINRA.java
@@ -10,7 +10,7 @@ For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the nu
  */
 public class FINRA {
     public static void main(String[] args) {
-       /*
+
         //print from 1 to 30 but
             for (int i = 1; i <= 30; i++) {
                 // divide 3 and 5 numbers
@@ -36,7 +36,7 @@ public class FINRA {
 
         }
 
-        */
+
 
         //creat for loop from 1 to 30
         for(int i = 1 ; i <= 30 ;i++) {
