29f067e5bd86dd3f5aba3a3aa83c82621c98930f
mariaCydeo <maria+mariapinarlii@gmail.com>
2024-09-28 22:56:34 +0300

    Added Task3 FINRA

---
+++ src/maria/Task3SwapTwoNum.java
@@ -0,0 +1,13 @@
+package maria;
+
+public class Task3SwapTwoNum {
+    public static void main(String[] args) {
+
+
+        // Swap two int variables' values without using a third variable
+
+
+
+
+    }
+}
