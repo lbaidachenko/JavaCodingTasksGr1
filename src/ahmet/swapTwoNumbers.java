88ee906508a5c00f7d87d49e9d28e13a850de6c1
Jomolocco <ahmet23cemal@gmail.com>
2024-10-01 23:31:14 +0200

    swap2number

--- src/ahmet/swapTwoNumbers.java
+++ src/ahmet/swapTwoNumbers.java
@@ -4,11 +4,10 @@ public class swapTwoNumbers {
     public static void main(String[] args) {
         int num1 = 1 ;
         int num2 = 2 ;
-        int num3 = num1;
 
-        num3 = num1 ;
-        num1 = num2 ;
-        num2 = num3 ;
+        num1 = num1 + num2 ;
+        num2 = num1 - num2 ;
+        num1 = num1 - num2 ;
 
         System.out.println("num1 = " + num1);
         System.out.println("num2 = " + num2);
