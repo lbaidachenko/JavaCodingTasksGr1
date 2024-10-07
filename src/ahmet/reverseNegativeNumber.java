ea63d6ce428ab55d90d91e258438824cfdb13db5
Jomolocco <ahmet23cemal@gmail.com>
2024-10-07 23:44:01 +0200

    reverseNegativeNumber

---
+++ src/ahmet/reverseNegativeNumber.java
@@ -0,0 +1,23 @@
+package ahmet;
+
+public class reverseNegativeNumber {
+    //Write a return method that can reverse digits of a negative number and return it as int
+    //input: -35  output: -53
+    public static void main(String[] args) {
+        reverse(-35);
+        reverse(-243);
+
+    }
+    public static void reverse(int input) {
+        System.out.println("Given input : "+input);
+       int output = 0;
+        while (input != 0 ){
+            int remainder = input % 10;
+            output = output * 10 + remainder;
+            input /= 10;
+        }
+        System.out.println("Reversed int : "+output);
+
+
+    }
+}
