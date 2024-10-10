5185b0e9ce32059313acff138a6aacc28b6b83cc
lbaidachenko <lsbaidachenko@gmail.com>
2024-10-10 21:52:29 +0100

    Task3 created

---
+++ src/luda/Task3.java
@@ -0,0 +1,14 @@
+package luda;
+/*
+ swapTwoNumbers:
+Swap two int variables' values without using a third variable
+ */
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void swapTwoNumbers(int num1, int num2){
+
+    }
+}
