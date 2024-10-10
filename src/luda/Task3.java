479d5f5396abe5e62fe2a28f861c9d15a4a6fcd5
lbaidachenko <lsbaidachenko@gmail.com>
2024-10-10 22:08:29 +0100

    Task3 completed

--- src/luda/Task3.java
+++ src/luda/Task3.java
@@ -5,10 +5,19 @@ Swap two int variables' values without using a third variable
  */
 public class Task3 {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("----------------------------------------");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
 
     }
 
-    public static void swapTwoNumbers(int num1, int num2){
-
-    }
 }
